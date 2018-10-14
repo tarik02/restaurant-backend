f877459aa1fc26ce8f4bfce8c4e635d617cfa41c
Tarik02 <Taras.Fomin@gmail.com>
2018-10-14 09:25:59 +0300

    Minus batches when ordering

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -6,11 +6,16 @@ use App\Services\ReviewsService;
 use App\Services\StorageService;
 use App\Util\OrderStatus;
 use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Connection;
+use Illuminate\Support\Collection;
 use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class OrderController extends Controller {
+  /** @var Connection */
+  private $db;
+
   /** @var ReviewsService */
   private $reviews;
 
@@ -20,6 +25,7 @@ class OrderController extends Controller {
   public function __construct(Container $container) {
     parent::__construct($container);
 
+    $this->db = $container['db']->connection();
     $this->reviews = $container['reviews'];
     $this->storage = $container['storage'];
   }
@@ -83,7 +89,7 @@ class OrderController extends Controller {
         $target['coordinates']['lat'],
         $target['coordinates']['lng']
       );
-      $id = $orders->insertGetId([
+      $orderId = $orders->insertGetId([
         'contact_name' => $info['name'],
         'phone' => $info['phone'],
 
@@ -100,20 +106,99 @@ class OrderController extends Controller {
         'storage_id' => $storageId,
       ]);
 
-      $ordersCourses->insert($cartItems->map(function ($item) use ($cart, $id) {
+      $ordersCourses->insert($cartItems->map(function ($item) use ($cart, $orderId) {
         return [
-          'order_id' => $id,
+          'order_id' => $orderId,
           'course_id' => $item['id'],
           'count' => $cart[$item['id']],
         ];
       })->toArray());
 
+      $coursesIngredients = $this->db->table('courses_ingredients')
+        ->whereIn('course_id', array_keys($cart))
+        ->get()
+        ->groupBy(['course_id', 'ingredient_id'])
+        ->map(function (Collection $ingredients) {
+          return $ingredients->map(function (Collection $ingredient) {
+            return $ingredient->first()['amount'];
+          });
+        })
+      ;
+      $uniqueIngredients = $coursesIngredients->map(function (Collection $ingredients) {
+        return $ingredients->keys();
+      })->values()->collapse()->unique();
+//      $ingredients = $this->db->table('ingredients')
+//        ->whereIn('id', $uniqueIngredients)
+//        ->get()
+//        ->pluck()
+
+//      throw new ResponseException($response->withJson(
+//
+//      , 500));
+      $ingredients = $uniqueIngredients->mapWithKeys(function ($id) {
+        return [$id => 0];
+      });
+      foreach ($cartItems as $item) {
+        $id = intval($item['id']);
+        $count = $cart[$id];
+
+        foreach ($coursesIngredients[$id] as $ingredient => $amount) {
+          $ingredients[$ingredient] += $count * $amount;
+        }
+      }
+
+      foreach ($ingredients as $id => $count) {
+        $result = $this->db->select(
+          <<<SQL
+SELECT
+  *
+FROM 
+  (SELECT
+    *
+  FROM
+    `storages_batches`
+  WHERE
+    `remaining` <> 0 AND
+    `ingredient_id` = :ingredient AND
+    `best_by` >= CURRENT_TIMESTAMP()
+  ORDER BY
+    `best_by`
+  ) r1
+JOIN
+  (SELECT @rn := 0) r2
+WHERE
+  (@rn := @rn + `remaining`) - `remaining` < :count
+SQL
+        , [
+          'ingredient' => $id,
+          'count' => $count,
+        ]);
+
+        foreach ($result as $batch) {
+          $taking = min($count, floatval($batch['remaining']));
+          $count -= $taking;
+          $this->db->update(
+            <<<SQL
+UPDATE
+  `storages_batches`
+SET
+  `remaining`=`remaining`-:taking
+WHERE
+  `id`=:id
+SQL
+          , [
+            'id' => $batch['id'],
+            'taking' => $taking,
+          ]);
+        }
+      }
+
       return $response->withJson([
         'status' => 'ok',
-        'order_id' => $id,
+        'order_id' => $orderId,
         'token' => $token,
       ]);
-    });
+    }, 5);
   }
 
   public function watch(Request $request, Response $response, array $args) {
